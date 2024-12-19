6b2cd8c024fd0478182a00da2db5bea3bd09ca31
AugustoDeveloper-py <augustodesigner26@gmail.com>
2024-12-19 07:25:23 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProfileLinks from './templates/ProfileLinks';
 import AboutMe from './templates/AboutMe';
 
